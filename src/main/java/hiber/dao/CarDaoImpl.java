b9942794b4d61e43bb35db5e46ec5985c73599dd
SadovNick <alecos100@gmail.com>
2022-12-13 16:07:22 +0300

    second commit

--- src/main/java/hiber/dao/CarDaoImpl.java
+++ src/main/java/hiber/dao/CarDaoImpl.java
@@ -24,14 +24,4 @@ public class CarDaoImpl implements CarDao {
         TypedQuery<Car> query = sessionFactory.getCurrentSession().createQuery("from Car ");
         return query.getResultList();
     }
-
-
-    @SuppressWarnings("unchecked")
-    public Car findCar(String car_name, String car_series) {
-        TypedQuery<Car> findCarQuery = sessionFactory.getCurrentSession().createQuery("from Car where name = :car_name and series = :car_series")
-                .setParameter("car_name", car_name)
-                .setParameter("car_series", car_series);
-        List<Car> findCarList = findCarQuery.getResultList();
-        return findCarList.get(0);
-    }
 }
